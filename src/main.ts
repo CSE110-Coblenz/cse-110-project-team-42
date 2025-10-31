6713a43fa8d48e628fe8c23e4d9a064b20220705
diegomez5 <dig005@ucsd.edu>
2025-10-31 15:46:07 -0700

    add 'your choice won'

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,8 @@ const game2Results = "\
 	RESULTS\n\n\n\n\
 	$5 Buy in:                    $3 Buy in:                    $10 Buy in:\n\
 	EV = +0.77                    EV = +1.15                    EV = +2.69\n\
-	1000 trials = +$7700     1000 trials = +$11500     1000 trials = +$26900"
+	1000 trials = +$7700     1000 trials = +$11500     1000 trials = +$26900\n\n\n\
+	Your choice won!"
 
 new Results({
   containerId: "container",
