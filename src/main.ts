1e010cab6b5ddf7be2408f385f06cbdedf0431b5
diegomez5 <dig005@ucsd.edu>
2025-10-31 15:43:00 -0700

    add second game message

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,18 @@ const game1Results = "\
 	🟢Green ➡️ +$545\n\
 	🔵Blue ➡️ -$120.8\n\
 	\n\
-	Your choice of green won!\
-	"
+	Your choice of green won!"
+
+const game2Results = "\
+	RESULTS\n\n\n\n\
+	$5 Buy in:                    $3 Buy in:                    $10 Buy in:\n\
+	EV = +0.77                    EV = +1.15                    EV = +2.69\n\
+	1000 trials = +$7700     1000 trials = +$11500     1000 trials = +$26900"
 
 new Results({
   containerId: "container",
-  resultText: game1Results,
+  resultText: game2Results,
   hearts: 3,
-  onProceed: () => alert("Proceed clicked!"),
+  onProceed: () => alert("clicked"),
 });
 
